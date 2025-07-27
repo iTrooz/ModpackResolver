2dfcc296e9850ca0625a8173c894b48921c93bc3
iTrooz <hey@itrooz.fr>
2025-07-27 17:59:13 +0200

    cleanup

--- mclib/src/finder/ISolutionFinder.ts
+++ mclib/src/finder/ISolutionFinder.ts
@@ -4,25 +4,20 @@ export interface ISolutionFinder {
     /**
      * Download releases for all mods, and return solutions that try to satisfy the constraints (Best solutions are returned first)
      * This is the method you should use for the main functionality of the class.
-     * @param mods List of mod IDs
+     * @param modIds List of mod IDs
      * @param constraints Constraints to apply on the solution
-     * @param nbSolution Max number of solutions to return
+     * @param nbSolutions Max number of solutions to return
      * @returns Array of solutions
      */
-    findSolutions(mods: string[], constraints?: Constraints, nbSolution?: number): Promise<Solution[]>;
+    findSolutions(modIds: string[], constraints?: Constraints, nbSolutions?: number): Promise<Solution[]>;
 
     /**
-     * Resolves mod IDs to their releases metadata
-     * @param modIds List of mod IDs to resolve
-     * @returns Array of ModAndReleases objects
+     * Resolves releases of mods
      */
     resolveMods(modIds: string[]): Promise<ModAndReleases[]>;
 
     /**
      * Finds the best Minecraft configurations (version + loader) that satisfy all mods
-     * @param mods List of mods with their releases
-     * @param nbSolution Maximum number of solutions to return
-     * @returns Array of compatible solutions
      */
     resolveSolutions(mods: ModAndReleases[], constraints: Constraints, nbSolution: number): Solution[];
 }
