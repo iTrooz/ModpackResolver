42cba86cbc8b462a4c93d9085ca056b54ea26476
iTrooz <hey@itrooz.fr>
2025-08-14 21:01:19 +0200

    chore: add "lint" script (#51)

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,16 +1,24 @@
-import { ModrinthRepository, CurseForgeRepository, ModQueryService, LocalSolutionFinder } from 'mclib';
+import {
+	ModrinthRepository,
+	CurseForgeRepository,
+	ModQueryService,
+	LocalSolutionFinder
+} from 'mclib';
 import type { ISolutionFinder } from 'mclib';
 declare const __APP_VERSION__: string;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const fetchClient = (url: any, init: RequestInit = {}) => {
-    init['headers'] = {
-        ...init['headers'],
-        'User-Agent': 'github.com/iTrooz/ModpackCreator v' + __APP_VERSION__
-    };
-    return fetch(url, init);
-}
+	init['headers'] = {
+		...init['headers'],
+		'User-Agent': 'github.com/iTrooz/ModpackCreator v' + __APP_VERSION__
+	};
+	return fetch(url, init);
+};
 
-export const repositories = [new ModrinthRepository(fetchClient), new CurseForgeRepository(fetchClient)];
+export const repositories = [
+	new ModrinthRepository(fetchClient),
+	new CurseForgeRepository(fetchClient)
+];
 export const modQueryService = new ModQueryService(repositories);
-export const solutionFinder: ISolutionFinder = new LocalSolutionFinder(modQueryService)
+export const solutionFinder: ISolutionFinder = new LocalSolutionFinder(modQueryService);
